604c21223b4d2fe553f0bb572bf55abcd450e6d5
rozhkov.sergei <rozhkov.sergei.2014@gmail.com>
2019-04-14 00:38:47 +0300

    Presentation Layer

---
+++ PresentationLayer/Models/DerictoryModel.cs
@@ -0,0 +1,18 @@
+﻿using DataLayer.Entites;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PresentationLayer.Models
+{
+    public class DerictoryViewModel : PageViewModel
+    {
+        public Directory Directory { get; set; }
+        public List<MaterialViewModel> Materials { get; set; }
+
+    }
+
+    public class DerictoryEditModel : PageEditModel
+    {
+    }
+}
---
+++ PresentationLayer/Models/MaterialModel.cs
@@ -0,0 +1,20 @@
+﻿using DataLayer.Entites;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PresentationLayer.Models
+{
+    public class MaterialViewModel : PageViewModel
+    {
+        public Material Material { get; set; }
+        public Material NextMaterial { get; set; }
+    }
+
+    public class MaterialEditModel : PageEditModel
+    {
+        [Required]
+        public int DerictoryId { get; set; }
+    }
+}
